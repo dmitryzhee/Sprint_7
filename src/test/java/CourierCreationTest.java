ade235348dcecd6f4172aad47c5b72f675dbb4f9
Dmitry Zhegalov <dmitryzhee@gmail.com>
2023-08-29 14:25:02 +0300

    Added CourierCreationTest

---
+++ src/test/java/CourierCreationTest.java
@@ -0,0 +1,34 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class CourierCreationTest {
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Test
+    public void courierCreationSuccess () {
+        Courier courier = new Courier("naruto1", "12345", "Ivan");
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(courier)
+                .when()
+                .post("/api/v1/courier");
+        response.then().assertThat().statusCode(201);
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+}
